4964c7f2f3dd652588ecc8123810bf6eac99983c
Andressa <isa.desenvolvimento@gmail.com>
2021-05-17 18:23:38 -0300

    teste

--- src/components/Character.tsx
+++ src/components/Character.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 import Card from '@/components/Card';
 import styles from '@/styles/Card.module.scss';
+import Link from "./Link";
 
 const Character = ({ characters }) => {
     return (
         <Card>
             {characters.map((character) => {
                 return (
-                    <div id={character.id} className={styles.box}>
-                        <img src={character.image} />
-                        <div className={styles.bgtext}>
-                            <label>{character.name}</label>
+                    <Link href={`/api/character/${character.id}`} key={character.id}>
+                        <div id={character.id} className={styles.box}>
+                            <img src={character.image} />
+                            <div className={styles.bgtext}>
+                                <label>{character.name}</label>
+                            </div>
                         </div>
-                    </div>
+                    </Link>
                 );
             })}
         </Card>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,9 @@ import { useRouter } from 'next/router';
 import Character from "../components/Character";
 import styles from '@/styles/Layout.module.scss';
 import { graphqlClient } from '@/pages/api/graphql';
-
+import Image from 'next/image';
+//import style from '@/styles/Item.module.scss';
+import style from '@/styles/Details.module.scss';
 
 
 export default function Home(results) {
@@ -46,8 +48,48 @@ export default function Home(results) {
         <title>Challenge ML</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className={style.wrapper}>
+        <div className={style.one}></div>
+        <div className={style.two}>
+          <div className={style.block}>
+            <div className={style.a}>
+              <div className={style.title}>ABOUT</div>
+            </div>
+            <div className={style.b}>
+              <div className={style.description}>Rick Sanchez is a male human.
+              He is alive and well. Last see. adasdas</div>
+            </div>
+          </div>
 
-      <div className={styles.container}>
+          <div className={style.block}>
+            <div className={style.a}>
+              <div className={style.title}>ABOUT</div>
+            </div>
+            <div className={style.b}>
+              <div className={style.description}>Rick Sanchez is a male human.
+              He is alive and well. Last see. adasdas</div>
+            </div>
+          </div>
+
+
+
+        </div>
+      </div>
+
+      {/* <div className={style.wrapper} id="1">
+        <div className={style.imageContainer}>
+          <Image width="180" height="180" src={"/assets/model.jpg"} objectFit="contain" />
+        </div>
+        <div className={style.containerDetails}>
+          <div className={style.priceContainer}>
+            <h2>
+              {"location"} <br /> {"origin"} <br /> {"episode"} <br />
+            </h2>
+          </div>
+        </div>
+      </div> */}
+
+      {/* <div className={styles.container}>
         <img src="/assets/image-logo.png" className={styles.logo}></img>
         <form
           onSubmit={(e) => handleSubmit(e)}
@@ -65,8 +107,8 @@ export default function Home(results) {
           </div>
         </form>
         <Character characters={characters} />
-      </div>
-    </div>
+      </div> */}
+    </div >
   );
 }
 
